5b1503845e49097b8dbf58bf21b2f61c2e9c71d0
kirai <hectorgarcia@gmail.com>
2018-02-09 15:23:52 +0900

    show video lenghts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss";
 
@@ -10,10 +11,19 @@ YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
 });
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos }); // When key and variable name has the same name
+									   // Alternative syntax: this.setState({ videos: videos});
+		});
+	}
 	render() {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos = {this.state.videos}/>
 			</div>
 		);
 	}
