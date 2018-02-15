6bfda2bb8a54b354dbc016896c3c98d24593d73f
kirai <hectorgarcia@gmail.com>
2018-02-15 14:04:38 +0900

    Debounce youtube search api calls to 300

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,23 +9,18 @@ class SearchBar extends Component {
 
 	render() {
 		return ( 
-			<div>
+			<div className="search_bar">
 				<input 
 					value = {this.state.term}
-					onChange={event => this.setState({ term: event.target.value})} />
+					onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		);
 	}
-}
 
-/* Event handler
-onInputChange(event) {
-console.log(event.target.value);
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
 }
-
-functional based component
-const SearchBar = () => {
-	return <input />;
-};*/
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
+const API_KEY = 'AIzaSyDO8SmQXAFI_945dYCbSDa5IUAKhmaLenY';
 
 class App extends Component {
   constructor(props) {
@@ -15,18 +16,25 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
+
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
